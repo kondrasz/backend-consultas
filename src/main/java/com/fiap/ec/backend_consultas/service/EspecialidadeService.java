e148cec85a6517a8041bb4b0d833a070d57e30e1
kondrasz <cjkondrasz@gmail.com>
2026-03-10 21:43:51 -0300

    feat: criação da entidade Paciente

--- src/main/java/com/fiap/ec/backend_consultas/service/EspecialidadeService.java
+++ src/main/java/com/fiap/ec/backend_consultas/service/EspecialidadeService.java
@@ -1,18 +1,29 @@
 package com.fiap.ec.backend_consultas.service;
+
 import com.fiap.ec.backend_consultas.model.Especialidade;
 import com.fiap.ec.backend_consultas.repository.EspecialidadeRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
+
 @Service
 public class EspecialidadeService {
+
     private final EspecialidadeRepository repository;
+
     public EspecialidadeService(EspecialidadeRepository repository) {
         this.repository = repository;
     }
+
     public Especialidade salvar(Especialidade especialidade) {
         return repository.save(especialidade);
     }
+
     public List<Especialidade> listar() {
         return repository.findAll();
+    }
+
+    public Especialidade buscarPorId(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Especialidade não encontrada"));
     }
 }
